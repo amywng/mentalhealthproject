5a5527c8d0ab7c55dd752c4bf33f74e0a143b255
Amy Wang <amy@lu-wang.com>
2022-02-18 20:27:38 +0000

    Changing to prototype website

---
+++ config/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+	  config.cloud_name = 'sciencefair'
+	    config.api_key = '311537544273765'
+	      config.api_secret = 'HcI_ro0L_1WnPgsY-KY3BNOg7J0'
+	        config.secure = true
+		  config.cdn_subdomain = true
+end
